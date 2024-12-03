2052b64c21ae8f5ad8dad68dfa12dfb690224f38
Anish Bhowmick <84652516+anishbhowmick@users.noreply.github.com>
2024-12-03 23:35:21 +0530

    JWT token added

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import { Doctor, Patient } from '../models/User.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
+
+// Replace 'your_jwt_secret_key' with a secure key in a real application
+const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/login', async (req, res) => {
   const { email, password, role } = req.body;
@@ -26,6 +30,16 @@ router.post('/login', async (req, res) => {
       return res.status(400).send({ error: 'Invalid credentials' });
     }
 
+    // Create payload for JWT
+    const payload = {
+      id: user._id,
+      name: `${user.firstName} ${user.lastName}`,
+      role: user.role,
+    };
+
+    // Generate JWT token
+    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
+
     const userData = {
       id: user._id,
       firstName: user.firstName,
@@ -34,7 +48,7 @@ router.post('/login', async (req, res) => {
       role: user.role,
     };
 
-    res.status(200).send({ message: 'Login successful', role: user.role, user: userData });
+    res.status(200).send({ message: 'Login successful', token, user: userData });
   } catch (error) {
     res.status(500).send({ error: 'Server error' });
   }
